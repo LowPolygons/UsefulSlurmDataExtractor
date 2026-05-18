27745e9cca8e59642467622c4e58b9485ee87cd6
Tristan Rimmer <Tristan Rimmer>
2026-05-18 11:57:01 +0100

    Small refactor in prep for full detail implementation

--- src/commands/detail.rs
+++ src/commands/detail.rs
@@ -30,60 +30,68 @@ pub fn command(
                 .position(|&item| item.eq(id))
                 .unwrap_or(job_ids.len() + 1)];
 
-            println!("==========================");
-            println!("Job Name & ID: {}, {}", target_job.name, target_job.job_id);
-            println!(
-                "User Name and ID: {}, {}",
-                target_job.user_name, target_job.user_id
-            );
-            println!("--------------------------");
-            println!(
-                "Submit Time: {}",
-                DateTime::from_timestamp(target_job.submit_time as i64, 0)
-                    .expect("Could not determine")
-            );
-            println!(
-                "Latest Start Time: {}",
-                DateTime::from_timestamp(target_job.start_time as i64, 0)
-                    .expect("Could not determine")
-            );
-            println!("Job status: {}", target_job.job_state);
-            println!("--------------------------");
-            println!(
-                "Job max length: {} hours",
-                target_job.time_limit.number / 60.0
-            );
-            println!("Number of nodes: {}", target_job.node_count.number);
-            println!(
-                "Number of tasks per node: {}",
-                target_job.tasks_per_node.number
-            );
-            println!("Job directory: {}", target_job.current_working_directory);
-
-            // Output file if it exists
-            let output_file = Path::new(&target_job.standard_output);
-
-            if output_file.try_exists().map_err(|_| ())? {
-                let lines = line_vec_from_file(&target_job.standard_output).map_err(|e| {
-                    println!("Error: {}", e);
-                    return ();
-                })?;
-
-                let mut num_hidden: i32 = 0;
-
-                lines.iter().enumerate().for_each(|(index, line)| {
-                    if index < 5 || index > lines.len() - 5 {
-                        println!("{}", line);
-                    } else {
-                        num_hidden = num_hidden + 1;
-
-                        if index == lines.len() - 6 {
-                            println!(".. {} lines hidden ..", num_hidden);
-                        }
-                    }
-                });
-            }
+            print_infomation_about_file(target_job).map_err(|e| {
+                println!("Error: {}", e);
+                return ();
+            })?;
         }
+    }
+    Ok(())
+}
+
+fn print_infomation_about_file(target_job: &SlurmJob) -> Result<(), String> {
+    println!("==========================");
+    println!("Job Name & ID: {}, {}", target_job.name, target_job.job_id);
+    println!(
+        "User Name and ID: {}, {}",
+        target_job.user_name, target_job.user_id
+    );
+    println!("--------------------------");
+    println!(
+        "Submit Time: {}",
+        DateTime::from_timestamp(target_job.submit_time as i64, 0).expect("Could not determine")
+    );
+    println!(
+        "Latest Start Time: {}",
+        DateTime::from_timestamp(target_job.start_time as i64, 0).expect("Could not determine")
+    );
+    println!("Job status: {}", target_job.job_state);
+    println!("--------------------------");
+    println!(
+        "Job max length: {} hours",
+        target_job.time_limit.number / 60.0
+    );
+    println!("Number of nodes: {}", target_job.node_count.number);
+    println!(
+        "Number of tasks per node: {}",
+        target_job.tasks_per_node.number
+    );
+    println!("Job directory: {}", target_job.current_working_directory);
+
+    // Output file if it exists
+    let output_file = Path::new(&target_job.standard_output);
+
+    if output_file
+        .try_exists()
+        .map_err(|_| String::from("Couldn't determine if output file exists"))?
+    {
+        let lines = line_vec_from_file(&target_job.standard_output).map_err(|e| {
+            return e;
+        })?;
+
+        let mut num_hidden: i32 = 0;
+
+        lines.iter().enumerate().for_each(|(index, line)| {
+            if index < 5 || index > lines.len() - 5 {
+                println!("{}", line);
+            } else {
+                num_hidden = num_hidden + 1;
+
+                if index == lines.len() - 6 {
+                    println!(".. {} lines hidden ..", num_hidden);
+                }
+            }
+        });
     }
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,8 @@ use crate::{
 use clap::Parser;
 
 pub fn json_string_to_struct<T: DeserializeOwned>(stringy_json: String) -> Result<T, ()> {
-    let structy_value = serde_json::from_str(&stringy_json).map_err(|_| {
+    let structy_value = serde_json::from_str(&stringy_json).map_err(|_e| {
+        // println!("{}", _e.to_string());
         return ();
     })?;
 
